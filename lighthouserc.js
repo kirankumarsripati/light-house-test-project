66e61763a0e1c59336fd079daf58608bd053730d
Ajit Kumar <63657725+ajitkumar2797@users.noreply.github.com>
2023-02-24 10:23:02 +0530

    Create lighthouserc.js

---
+++ lighthouserc.js
@@ -0,0 +1,8 @@
+  ci: {
+    upload: {
+      target: 'lhci',
+      serverBaseUrl: 'http://18.200.44.239:9001/',
+      token: '2fa267db-34a9-484e-b241-878210fa798f', // could also use LHCI_TOKEN variable instead
+    },
+  },
+};
