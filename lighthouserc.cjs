7e02e3fd2a8dc87e8528556decefd9974abfc288
Ajit Kumar <63657725+ajitkumar2797@users.noreply.github.com>
2023-02-24 13:31:24 +0530

    Update and rename lighthouserc.js to lighthouserc.cjs

---
+++ lighthouserc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    ci: {
+      collect: {
+        puppeteerScript: 'lighthouse/generate-stats.js',  // Ensure there's an authenticated user before running Lighthouse
+        puppeteerLaunchOptions: {  
+          defaultViewport: null  
+        }
+    },    
+      
+      
+      upload: {
+        target: 'lhci',
+        serverBaseUrl: 'http://18.200.44.239:9001/',
+        token: '5d8f69f7-7813-4c40-86b5-47ff5eb513bf', // could also use LHCI_TOKEN variable instead
+      },
+    },
+  };
+  
