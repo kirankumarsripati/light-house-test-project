b786e80de22224c203591f54b758391746494eb2
Ajit Kumar <63657725+ajitkumar2797@users.noreply.github.com>
2023-02-24 10:28:55 +0530

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@
     upload: {
       target: 'lhci',
       serverBaseUrl: 'http://18.200.44.239:9001/',
-      token: '2fa267db-34a9-484e-b241-878210fa798f', // could also use LHCI_TOKEN variable instead
+      token: '5d8f69f7-7813-4c40-86b5-47ff5eb513bf', // could also use LHCI_TOKEN variable instead
     },
   },
 };
